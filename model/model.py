8b6c3ee99312c60390aeaa1735d90cfd3a4a6e9c
littlewheaty <982770372@qq.com>
2026-03-07 15:46:54 +0800

    model拼装

--- model/model.py
+++ model/model.py
@@ -1,4 +1,5 @@
 import math
+from typing import Optional, Tuple
 
 from transformers import PretrainedConfig
 
@@ -74,6 +75,10 @@ class MindConfig(PretrainedConfig):
         )
 import torch
 import torch.nn as nn
+import math
+from torch.nn import functional as F
+from transformers.activations import ACT2FN
+
 #集成nn.Module类
 class RMSNorm(nn.Module):
     def __init__(self,dim:int,eps:float=1e-5):
@@ -147,5 +152,193 @@ def repeat_kv(x:torch.Tensor, n_rep:int)->torch.Tensor:
 class Attention(nn.Module):
     def __init__(self, args:MindConfig):
         super().__init__()
-        self.num_attention_heads = args.num_attention_heads
+        self.num_key_value_heads = args.num_key_value_heads if args.num_key_value_heads is not None else args.num_attention_heads
         
+        assert args.num_attention_heads % self.num_key_value_heads == 0, "num_attention_heads must be divisible by num_key_value_heads"
+        
+        self.n_local_heads = args.num_attention_heads
+        self.num_key_value_heads = args.num_key_value_heads
+        self.n_rep = self.n_local_heads // self.num_key_value_heads
+        self.head_dim = args.hidden_size // args.num_attention_heads
+        
+        self.q_proj = nn.Linear(args.hidden_size, args.num_attention_heads * self.head_dim, bias = False)
+        self.k_proj = nn.Linear(args.hidden_size, self.num_key_value_heads * self.head_dim, bias = False)
+        self.v_proj = nn.Linear(args.hidden_size, self.num_key_value_heads * self.head_dim, bias = False)
+        
+        self.o_proj = nn.Linear(args.num_attention_heads * self.head_dim, args.hidden_size, bias = False)
+        
+        self.attn_dropout = nn.Dropout(args.dropout)
+        self.resid_dropout = nn.Dropout(args.dropout)
+        self.dropout = args.dropout
+        
+        self.flash = hasattr(torch.nn.functional, "scaled_dot_product_attention") and args.flash_attention
+        
+    def forward(self, 
+                x:torch.Tensor, 
+                position_embedding:Tuple[torch.Tensor, torch.Tensor],
+                past_key_value:Optional[Tuple[torch.Tensor, torch.Tensor]] = None,
+                use_cache:bool = False,
+                attention_mask:Optional[torch.Tensor] = None,
+                
+    )->torch.Tensor:
+        bsz, seq_len = x.shape
+        xq, xk, xv = self.q_proj(x), self.k_proj(x), self.v_proj(x)
+        q = xq.view(bsz, seq_len, self.n_local_heads, self.head_dim)
+        k = xk.view(bsz, seq_len, self.num_key_value_heads, self.head_dim)
+        v = xv.view(bsz, seq_len, self.num_key_value_heads, self.head_dim)
+        
+        cos, sin = position_embedding
+        xq, xk = apply_rotary_pos_emb(xq, xk, cos[:seq_len], sin[:seq_len])
+        if past_key_value is not None:
+            xk = torch.cat((past_key_value[0], xk), dim=1)
+            xv = torch.cat((past_key_value[1], xv), dim=1)
+        past_kv = (xk, xv) if use_cache else None
+        
+        xq, xk, xv = (
+            xq.transpose(1, 2),
+            # [bsz, n_local_heads, seq_len, head_dim]
+            repeat_kv(xk, self.n_rep).transpose(1, 2),
+            repeat_kv(xv, self.n_rep).transpose(1, 2),
+        )
+        if self.flash and seq_len > 1 and (attention_mask is None or torch.all(attention_mask == 1)):
+            attn_mask = (
+                None
+                if attention_mask is None
+                else attention_mask.view(bsz, 1, 1, -1).expand(bsz, self.n_local_heads, seq_len, -1).bool()
+            )
+            output = F.scaled_dot_product_attention(
+                xq, xk, xv, attn_mask = attn_mask, dropout_p = self.dropout if self.training else 0.0, is_causal = True
+            )
+        else:
+            scores = (xq @ xk.transpose(-2, -1)) / math.sqrt(self.head_dim)
+            scores = scores + torch.triu(
+                torch.full_like((seq_len, seq_len), float("-inf"), device = scores.device),
+                diagonal = 1
+            ).unsqueeze(0).unsqueeze(0)
+            
+            if attention_mask is not None:
+                extended_attention_mask = attention_mask.unsqueeze(1).unsqueeze(2)
+                extended_attention_mask = (1.0 - extended_attention_mask) * -1e9
+                scores = scores + extended_attention_mask
+            scores = F.softmax(scores.float(), dim=-1).type_as(xq)
+            scores = self.attn_dropout(scores)
+            output = scores @ xv
+        # [bsz, n_local_heads, seq_len, head_dim]
+        output = output.transpose(1, 2).reshape(bsz, seq_len, -1)
+        output = self.resid_dropout(self.o_proj(output))
+        return output, past_kv    
+class FeedForward(nn.Module):
+    #初始化
+    def __init__(self, args:MindConfig):
+        super().__init__()
+        if args.intermediate_size is None:
+            intermediate_size = int(args.hidden_size * 8 / 3)
+            args.intermediate_size = 64 * ((intermediate_size + 64 - 1) // 64)
+        self.up_proj = nn.Linear(args.hidden_size, args.intermediate_size, bias = False)
+        self.down_proj = nn.Linear(args.intermediate_size, args.hidden_size, bias = False)
+        self.gate_proj = nn.Linear(args.hidden_size, args.intermediate_size, bias = False)
+        self.dropout = nn.Dropout(args.dropout)
+        self.act_fn = ACT2FN[args.hidden_act]
+    
+    def forward(self, x):
+        return self.dropout(
+            self.down_proj(self.act_fn(self.gate_proj(x) * self.up_proj(x)))
+        )
+
+class MindBlock(nn.Module):
+    def __init__(self, layer_id: int, config:MindConfig):
+        super().__init__()
+        self.num_attention_heads = config.num_attention_heads
+        self.hidden_size = config.hidden_size
+        self.head_dim = config.hidden_size // config.num_attention_heads
+        self.self_attn = Attention(config)
+        
+        self.layer_id = layer_id
+        self.input_layernorm = RMSNorm(config.hidden_size, eps = config.rms_norm_eps)
+        self.post_attention_layernorm = RMSNorm(config.hidden_size, eps = config.rms_norm_eps)
+        self.mlp = FeedForward(config)
+    
+    def forward(self, hidden_states, position_embedding, past_key_value = None, use_cache = False, attention_mask = None):
+        residual = hidden_states
+        hidden_states, present_key_value = self.self_attn(
+            self.input_layernorm(hidden_states), 
+            position_embedding, 
+            past_key_value, 
+            use_cache, 
+            attention_mask,
+        )
+        hidden_states = residual + hidden_states
+        hidden_states = hidden_states + self.mlp(self.post_attention_layernorm(hidden_states))
+        return hidden_states, present_key_value
+    
+class MindModel(nn.Module):
+    def __init__(self, config:MindConfig):
+        super().__init__()
+        self.vocab_size, self.num_hidden_layers = (
+            config.vocab_size,
+            config.num_hidden_layers,
+        )
+        
+        self.embed_tokens = nn.Embedding(config.vocab_size, config.hidden_size)
+        self.dropout = nn.Dropout(config.dropout)
+        
+        self.layers = nn.ModuleList(
+            [MindBlock(i, config) for i in range(config.num_hidden_layers)]
+        )
+        
+        self.norm = RMSNorm(config.hidden_size, eps = config.rms_norm_eps)
+        
+        #RoPE预计算
+        freqs_cos, freqs_sin = precompute_freqs_cis(
+            dim = config.hidden_size // config.num_attention_heads,
+            end = config.max_position_embeddings,
+            rope_base = config.rope_theta,
+            rope_scaling = config.rope_scaling,
+        )
+        self.register_buffer("freqs_cos", freqs_cos, persistent = False)
+        self.register_buffer("freqs_sin", freqs_sin, persistent = False)
+        
+    def forward(
+        self,
+        input_ids:Optional[torch.Tensor] = None,
+        attention_mask:Optional[torch.Tensor] = None,
+        past_key_values:Optional[Tuple[Tuple[torch.Tensor]]] = None,
+        use_cache:bool = False,
+        **kwargs,
+    ):
+        batch_size, seq_len = input_ids.shape
+        
+        if hasattr(past_key_values, 'layers'):
+            past_key_values = None
+        
+        past_key_values = past_key_values or [None] * len(self.layers)
+        
+        start_pos = (
+            past_key_values[0][0].shape[1] if past_key_values[0] is not None else 0
+        )
+        
+        hidden_states = self.dropout(self.embed_tokens(input_ids))
+        
+        position_embeddings = (
+            self.freqs_cos[start_pos : start_pos + seq_len], 
+            self.freqs_sin[start_pos : start_pos + seq_len]
+        )
+        
+        presents = []
+        for layer_idx, (layer, past_key_value) in enumerate(
+            zip(self.layers, past_key_values)
+        ):
+            hidden_states, presents = layer(
+                hidden_states,
+                position_embeddings,
+                past_key_value = past_key_value,
+                use_cache = use_cache,
+                attention_mask = attention_mask,
+            )
+            
+            presents.append(presents)
+            
+        hidden_states = self.norm(hidden_states)
+        
+        return hidden_states, presents
+                
